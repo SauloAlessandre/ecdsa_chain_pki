3b764d6c53c61568847bad19ff267af38cb9fc01
Saulo Alessandre <saulo.alessandre@gmail.com>
2019-08-22 10:27:12 -0300

    change params location

--- create_roots.sh
+++ create_roots.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 
-unit="sevin"
+# change this params
+unit="mywork"
+DIGEST=sha512
+VALID=3650
+OrganizationalUnit="/C=BR/ST=DF/L=Brasilia/O=TSE/OU=STI/OU=CSELE/OU=${unit^^}"
+OrganizationalServer="tse.jus.br"
+
 if [ "x$1" == "x-h" ]; then
     printf "use: $0 [curve_size]\n"
     printf "\tex: $0 256\n"
@@ -23,8 +29,10 @@ else
     CURVE=$1
 fi
 
+CURVE_NAME=secp${CURVE}r1
 CURVEDIR=ca-${CURVE}
 INTERDIR=ca_inter
+YEAR=`date +"%Y"`
 echo -e "${ECHO_RED}=================== Creating ${CURVEDIR}/ ===================${ECHO_END}"
 mkdir -p ${CURVEDIR}
 cd ${CURVEDIR}
@@ -34,43 +42,39 @@ mkdir -p ${INTERDIR}/certs ${INTERDIR}/crl ${INTERDIR}/csr ${INTERDIR}/newcerts 
 touch index.txt ${INTERDIR}/index.txt
 echo 1000 > serial; echo 2219752222 > ${INTERDIR}/serial; 
 
-CURVE_NAME=secp${CURVE}r1
-DIGEST=sha512
-VALID=3650
-OrganizationalUnit="/C=BR/ST=DF/L=Brasilia/O=TSE/OU=STI/OU=CSELE/OU=SEVIN"
 CA_KEY=private/ca-${unit}-${CURVE}.key.pem
 CA_CER=certs/ca-${unit}-${CURVE}.x509
 
-#especificando o arquivo de configuração do openssl
+#create openssl config
 cat ../openssl.cnf.tmpl | sed -e "s|PROJECT_DIR|$PWD|" -e "s|POLICY_TYPE|policy_strict|g" > openssl.cnf
 
-#criando uma chave ecdsa 256 cifrada
-shmsg "Criando chave secreta ${ECHO_BOLD}${CA_KEY}${ECHO_END} para ROOT-CA"
+#create ciphered ECDSA secret key
+shmsg "Creating secret key for ROOT-CA ${ECHO_BLUE}${CA_KEY}${ECHO_END}."
 while [ true ]; do
-    shmsg "Informe uma senha nova."
+    shmsg "Enter the new pass."
     openssl ecparam -genkey -name ${CURVE_NAME} | openssl ec -aes256 -out ${CA_KEY}
     error=$?
     if [ $error -ne 0 ]; then
-        shmsg "OCORREU UM ERRO ${error}"
-        read -n1 -p "Tentar novamente?"
+        shmsg "Something is wrong ${error}"
+        read -n1 -p "Try again?"
         continue
     fi
     break
 done
 
-shmsg "Criando certificado X509 ${CA_CER}"
-shmsg "informe a senha ROOT-CA da chave ${ECHO_BOLD}${CA_KEY}${ECHO_END}."
-#criando o certificado da SEVIN CA
+shmsg "Creating X509 certificate for ROOT-CA ${CA_CER}"
+shmsg "Enter pass for ROOT-CA ${ECHO_BLUE}${CA_KEY}${ECHO_END}."
+#create root certificate
 openssl req -config openssl.cnf -extensions v3_ca -new -key ${CA_KEY} -${DIGEST} -x509 -days ${VALID} \
-    -out ${CA_CER} -subj "${OrganizationalUnit}/CN=SEVIN-CA2019-${CURVE}/emailAddress=sevin@tse.jus.br"
-read -n 1 -p "Exibir CER X509 [${CA_CER}] (Y/n)? " opt; opt=`echo $opt | tr [:upper:] [:lower:]`
-if [ "x-${opt}" == "x-" ] || [ "x-${opt}" == "x-y" ]; then
+    -out ${CA_CER} -subj "${OrganizationalUnit}/CN=${unit^^}-CA${YEAR}-${CURVE}/emailAddress=${unit}@${OrganizationalServer}"
+read -n 1 -p "Show X509 certificate [${CA_CER}] (Y/n)? " opt;
+if [ "x-${opt}" == "x-" ] || [ "x-${opt,}" == "x-y" ]; then
     openssl x509 -text -noout -in ${CA_CER}
 else
     echo "ok"
 fi
 
-#definindo os nomes
+#intermediate ca
 UNIT_KEY=${INTERDIR}/private/ca-intermediate-${unit}-${CURVE}.key.pem
 UNIT_CSR=${INTERDIR}/csr/ca-intermediate-${unit}-${CURVE}.csr
 UNIT_CER=${INTERDIR}/certs/ca-intermediate-${unit}-${CURVE}.x509
@@ -78,41 +82,46 @@ UNIT_CER=${INTERDIR}/certs/ca-intermediate-${unit}-${CURVE}.x509
 cat ../openssl.cnf.tmpl | sed -e "s|PROJECT_DIR|${PWD}/${INTERDIR}|" -e "s|POLICY_TYPE|policy_loose|g" > openssl-i.cnf
 
 #-----------------------------------------------------
-#criando a chave ECDSA
-shmsg "Criando chave secreta para CA-INTER."
-shmsg "informe uma senha nova para ${ECHO_BLUE}${UNIT_KEY}${ECHO_END}."
-openssl ecparam -genkey -name ${CURVE_NAME} | openssl ec -aes256 -out ${UNIT_KEY}
-#criando o CSR intermediária
-shmsg "Criando requisição de certificado X509 ${UNIT_CSR}"
-shmsg "informe a senha de CA-INTER ${ECHO_BLUE}${UNIT_KEY}${ECHO_END}"
+#create ciphered ECDSA secret key
+shmsg "Creating secret key for CA-INTER ${ECHO_BLUE}${UNIT_KEY}${ECHO_END}."
+while [ true ]; do
+    shmsg "Enter the new pass."
+    openssl ecparam -genkey -name ${CURVE_NAME} | openssl ec -aes256 -out ${UNIT_KEY}
+    error=$?
+    if [ $error -ne 0 ]; then
+        shmsg "Something is wrong ${error}"
+        read -n1 -p "Try again?"
+        continue
+    fi
+    break
+done
+
+#create intermediate CSR
+shmsg "Creating X509 CSR ${UNIT_CSR}"
+shmsg "Enter pass for CA-INTER ${ECHO_BLUE}${UNIT_KEY}${ECHO_END}"
 openssl req -new -${DIGEST} -key ${UNIT_KEY} -out ${UNIT_CSR} \
-    -subj "${OrganizationalUnit}/CN=SEVIN-CA2019 Intermediate-${CURVE}/emailAddress=sevin@tse.jus.br"
-#verificando o CSR
-read -n 1 -p "Exibir CSR x509 [${UNIT_CSR}] (Y/n)? " opt; opt=`echo $opt | tr [:upper:] [:lower:]`
-if [ "x-${opt}" == "x-" ] || [ "x-${opt}" == "x-y" ]; then
+    -subj "${OrganizationalUnit}/CN=${unit^^}-CA${YEAR} Intermediate-${CURVE}/emailAddress=${unit}@${OrganizationalServer}"
+read -n 1 -p "Show x509 CSR [${UNIT_CSR}] (Y/n)? " opt; 
+if [ "x-${opt}" == "x-" ] || [ "x-${opt,}" == "x-y" ]; then
     openssl req -text -noout -in ${UNIT_CSR}
 else
     echo "ok"
 fi
 
 #criando o certificado intermediário
-shmsg "Criando certificado X509 ${UNIT_CER}"
-shmsg "informe a senha de ROOT-CA ${ECHO_BLUE}${CA_KEY}${ECHO_END}"
+shmsg "Creating X509 intermediate certificate ${UNIT_CER}"
+shmsg "Enter pass for ROOT-CA ${ECHO_BLUE}${CA_KEY}${ECHO_END}"
 openssl ca -config openssl-i.cnf -extensions v3_intermediate_ca -keyfile ${CA_KEY} \
     -cert ${CA_CER} -days ${VALID} -notext -md ${DIGEST} -in ${UNIT_CSR} -out ${UNIT_CER}
-read -n 1 -p "Exibir CER X509 [${UNIT_CER}] (Y/n)? " opt; opt=`echo $opt | tr [:upper:] [:lower:]`
-if [ "x-${opt}" == "x-" ] || [ "x-${opt}" == "x-y" ]; then
+read -n 1 -p "Show x509 CER [${UNIT_CER}] (Y/n)? " opt
+if [ "x-${opt}" == "x-" ] || [ "x-${opt,}" == "x-y" ]; then
     openssl x509 -text -noout -in ${UNIT_CER}
 else
     echo "ok"
 fi
 
-cat ${CA_CER} ${UNIT_CER} > ../sevin-ca.x509
 shmsg "${ECHO_BLUE}well done!${ECHO_END}"
-shmsg "lembre-se de copiar a lista de certificados sevin-ca.x509"
-shmsg "para o kernel linux/certs"
 
 rm openssl.cnf openssl-i.cnf
 
 cd ..
-
